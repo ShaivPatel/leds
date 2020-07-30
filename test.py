4c005df5655152850a43501d926897ef3b971690
ShaivPatel <shaivpatel410@gmail.com>
2020-07-30 18:15:53 -0400

    colorpicker test

--- test.py
+++ test.py
@@ -3,10 +3,39 @@
 #
 # Direct port of the Arduino NeoPixel library strandtest example.  Showcases
 # various animations on a strip of NeoPixels.
+import time
+from neopixel import *
+import argparse
+
+# LED strip configuration:
+LED_COUNT      = 600    # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
-import board
-import neopixel
-pixels = neopixel.NeoPixel(board.D18, 30)
 
-pixels[0] = (255, 0, 0)
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+r = 255
+g = 255
+b = 255
+
+while True:
+    color = Color(r,g,b)
+    colorWipe()
+    r = int(input('R:'))
+    g = int(input('G:'))
+    b = int(input('B:'))
+
+    print(f'Color chosen: {r}-{g}-{b}')
