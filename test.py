56a2cbbe12f4e0080b09e7f5298222706d3970f8
ShaivPatel <shaivpatel410@gmail.com>
2020-08-01 10:43:21 -0400

    add wheel

--- test.py
+++ test.py
@@ -17,7 +17,54 @@ LED_BRIGHTNESS = 75    # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
 
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
 
 # Define functions which animate LEDs in various ways.
 def colorWipe(strip, color, pixelCount= LED_COUNT, wait_ms=50):
@@ -52,14 +99,15 @@ if __name__ == '__main__':
     r = 255
     g = 255
     b = 255
-    count = 300
+    count = 255
 
     while True:
+        wheel(count)
         color = Color(g,r,b)
         colorWipe(strip,color, count,  5)
-        r = int(input('R:'))
-        g = int(input('G:'))
-        b = int(input('B:'))
+        # r = int(input('R:'))
+        # g = int(input('G:'))
+        # b = int(input('B:'))
         count = int(input('count:'))
 
         print('Color chosen: {%d}-{%d}-{%d}'%(r,g,b))
