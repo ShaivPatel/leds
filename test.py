57f1971d799f4d1e9c35630fe64732f78ce9699f
ShaivPatel <shaivpatel410@gmail.com>
2020-08-01 19:17:59 -0400

    add snake

--- test.py
+++ test.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
     strips = []
     try:
 
-        for i in range(1):
+        for i in range(2):
             strip = StripSegment(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, i*150, i*150+149)
             strip.begin()
             strips.append(strip)
