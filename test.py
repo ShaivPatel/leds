03f7f68dae06315b90f0a10cfceb9829103e75a9
ShaivPatel <shaivpatel410@gmail.com>
2020-08-01 14:35:45 -0400

    no message

--- test.py
+++ test.py
@@ -115,4 +115,4 @@ if __name__ == '__main__':
 
     except KeyboardInterrupt:
         if args.clear:
-            colorWipe(strip, Color(0, 0, 0), 10)
+            colorWipe(strip, Color(0, 0, 0), 50)
