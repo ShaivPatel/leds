c0033be52e6d5e095a85152ef8d3dd10858a4628
ShaivPatel <shaivpatel410@gmail.com>
2021-03-21 19:20:18 -0400

    add lcd

--- lights/lcdController.py
+++ lights/lcdController.py
@@ -5,7 +5,7 @@ lcd = Adafruit_CharLCD(rs=26, en=19,
                        d4=13, d5=6, d6=5, d7=11,
                        cols = 16, lines = 2)
 lcd.clear()
-
-lcd.message('Brightness\n   100%   ')
-sleep(3)
+while True:
+    lcd.message('Brightness\n   100%   ')
+# sleep(3)
 
