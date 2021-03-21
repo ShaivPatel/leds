3d5605e5997848a3a31c82310e0d2c6e7f9d8b01
ShaivPatel <shaivpatel410@gmail.com>
2021-03-21 19:30:21 -0400

    add lcd

--- lights/lcdController.py
+++ lights/lcdController.py
@@ -2,7 +2,7 @@ from time import sleep
 from Adafruit_CharLCD import Adafruit_CharLCD
 
 lcd = Adafruit_CharLCD(rs=26, en=19,
-                       d4=13, d5=6, d6=5, d7=11,
+                       d4=13, d5=6, d6=5, d7=18,
                        cols = 16, lines = 2)
 lcd.clear()
 while True:
