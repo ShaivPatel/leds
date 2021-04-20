1010006fe7cab445bc9758e0d8f26eaa52aadfa4
ShaivPatel <shaivpatel410@gmail.com>
2021-04-20 16:44:50 -0400

    add color

--- lights/controller.py
+++ lights/controller.py
@@ -104,7 +104,7 @@ class Controller:
                         theaterChaseRainbow(self.strip,25)
 
                 elif self.status == 'Ambient':
-                    colorWipe(self.strip, Color(100, 100, 100), 1)
+                    colorWipe(self.strip, Color(0,0,0,100), 1)
                 elif self.status == 'Color':
                     colorWipe(self.strip, self.color, 1)
                 elif 'Music' in self.status:
